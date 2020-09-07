d66a0444e36b075bfa1dc71b03719bd3fbc208d2
html5exe <css3studio@gmail.com>
2020-09-07 12:36:14 +0800

    메인페이지 1.디자인 일부 적용 2.카드 가로 스와이퍼 모드 3.텍스트 세로 스와이퍼 모드 4.상하 스와이프 아웃 기능 5.컨텐츠 보기 기능 6.키워드 추가 기능 7.키워드 선택 기능

--- js/dd.js
+++ js/dd.js
@@ -1,11 +1,13 @@
 
+var $$ = Dom7;
+
 var app = new Framework7({
   // App root element
   root: '#app',
   // App Name
-  name: 'My App',
+  name: 'DifferentDoors',
   // App id
-  id: 'com.myapp.test',
+  id: 'com.myapp.dd',
   // Enable swipe panel
   panel: {
     swipe: 'left',
@@ -22,12 +24,19 @@ var app = new Framework7({
 
 var mainView = app.views.create('.view-main');
 
-var swiper = app.swiper.create('.ib01', {
+/* 메인 스와이퍼 */
+var swiper_mode = "hor";
+var current_swiper_index = Math.floor($$('.ib01 .swiper-slide').length / 2);
+var swiper = create_swiper(swiper_mode,current_swiper_index);  //초기접속시 가로모드
+//스와이퍼 생성
+function create_swiper(mode,current){
+  if(mode == "hor"){  // 가로모드
+    return app.swiper.create('.ib01', {
       effect: 'coverflow',
       grabCursor: true,
       centeredSlides: true,
       slidesPerView: 'auto',
-      initialSlide: 5,
+      initialSlide: current,
       coverflowEffect: {
         rotate: 0,
         stretch: -2,
@@ -37,30 +46,155 @@ var swiper = app.swiper.create('.ib01', {
       },
       on: {
         tap: function () {
-          document.querySelector('.swiper-container').classList.add('expanded');
-          document.querySelector('.swiper-slide-active .tc01').classList.remove('draggable');
+          $$('.swiper-container').addClass('expanded');
+          $$('.swiper-slide-active .tc01').removeClass('draggable');
           this.detachEvents();
         },
       },
+    });
+  }else{  //세로모드
+    return app.swiper.create('.ib01', {
+      direction: 'vertical',
+      effect: 'coverflow',
+      autoHeight: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      initialSlide: current,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: -5,
+        depth: 15,
+        modifier: 15,
+        slideShadows: false,
+      },
+      on: {
+        tap: function () {
+          $$('.swiper-container').addClass('expanded');
+          this.detachEvents();
+        },
+      },
+    });
+  }
+}
+
+/* 스와이프 모드 변환 */
+$$('.navbar .right a.cList').on('click', function(){
+  //draggable.unset();
+  var current = swiper.activeIndex;
+  swiper.destroy();
+  swiper = create_swiper("ver",current);
+  $$(this).parent().removeClass("horMode").addClass("verMode");
+})
+$$('.navbar .right a.cCar').on('click', function(){
+  //draggable.set();
+  var current = swiper.activeIndex;
+  swiper.destroy();
+  swiper = create_swiper("hor",current);
+  $$(this).parent().removeClass("verMode").addClass("horMode");
+})
+
+
+/* 컨텐츠 닫기*/
+$$('.ib01 a.close').on('click', function(){
+  $$('.swiper-slide-active .tc01').scrollTop = 0;
+  $$('.swiper-container').removeClass('expanded');
+  $$('.swiper-slide-active .tc01').addClass('draggable');
+  $$("#add-new-keyword").hide();
+  swiper.attachEvents();
+})
+
+/* 하단 탭바 */
+$$('.toolbar-bottom a').each(function(){
+    $$(this).on('click', function(){
+      location.href = $$(this).attr('href');
+    });
 });
 
+//새 답안 추가 폼 열기
+$$('.ng01 li.add > button').on('click', function(){
+  $$("#add-new-keyword").show();
+  $$("#add-new-keyword").find("input").val('');
+  $$("#add-new-keyword").find("input").focus();
+  $$(".tc01 form button.confirm").css('display','none');
+});
 
-var elem = document.querySelector('.ib01 a.close');
-if (elem) {
-  elem.addEventListener('click', function(){
-    document.querySelector('.swiper-slide-active .tc01').scrollTop = 0;
-    document.querySelector('.swiper-container').classList.remove('expanded');
-    document.querySelector('.swiper-slide-active .tc01').classList.add('draggable');
-    swiper.attachEvents();
-  })
-}
+//새 답안 추가
+$$("#add-new-keyword").submit(function(event){
+  event.preventDefault();
+  var word = $$("#add-new-keyword input").val();
+  if(word == ''){
+    notification1.open();
+    $$("#add-new-keyword").find("input").focus();
+    return false;
+  }
+  var q_name = $$('.swiper-slide-active ul.ng01 li input').attr('name');
+  var opt_no = $$('.swiper-slide-active ul.ng01 li').length + 1;
+  var id = q_name+'-'+opt_no ;
+  var answer = $$("<li/>");
+  answer.append('<input type="radio" name="'+q_name+'" id="'+id+'" value="'+word+'"/>');
+  answer.append('<label class="btn02" for="'+id+'"><span>'+word+'</span></label>');
+  answer.insertBefore($$('.swiper-slide-active ul.ng01 li.add'));
+  $$("#add-new-keyword").hide();
+  $$(".tc01 form button.confirm").css('display','');
+  return false;
+});
 
-const buttons = document.querySelectorAll(".toolbar-bottom a")
-for (const button of buttons) {
-    button.addEventListener('click', function(){
-      location.href = this.getAttribute('href');
-    });
-}
+//답안 선택 저장 및 목록으로 돌아가기
+var dialog = app.dialog.create({
+  text: '나의 기록에 담았어요!',
+  content: '<br/><i class="xi-file-check" style="font-size:40px"></i>',
+  on: {
+    opened: function () {
+    }
+  }
+})
+
+$$(".tc01 form").submit(function(event){
+  event.preventDefault();
+  if($$(this).find('input[type="radio"]:checked').length < 1){
+    notification2.open();
+    return false;
+  }
+
+  /*여기에 저장 프로시져 추가*/
+
+  //notificationSave.open();
+  dialog.open();
+  setTimeout(function () {
+    dialog.close();
+  }, 1000);
+  $$('.swiper-slide-active .tc01').scrollTop = 0;
+  $$('.swiper-container').removeClass('expanded');
+  $$('.swiper-slide-active .tc01').addClass('draggable');
+  swiper.attachEvents();
+});
+
+// Create full-layout notification
+var notification1 = app.notification.create({
+  icon: '<i class="xi-info"></i>',
+  title: '키워드 추가',
+  //titleRightText: 'now',
+  subtitle: '키워드를 입력해 주세요',
+  //text: 'This is a simple notification message',
+  closeTimeout: 3000,
+});
+var notification2 = app.notification.create({
+  icon: '<i class="xi-info"></i>',
+  title: '나의 기록 추가',
+  //titleRightText: 'now',
+  subtitle: '키워드를 선택해 주세요',
+  //text: 'This is a simple notification message',
+  closeTimeout: 3000,
+});
+var notificationSave = app.notification.create({
+  icon: '<i class="xi-cloud-download"></i>',
+  title: '나의 기록 추가',
+  //titleRightText: 'now',
+  subtitle: '나의 기록에 담았어요!',
+  //text: 'This is a simple notification message',
+  closeTimeout: 3000,
+});
+
 
 /* 위아래로 스와이프 아웃 */
 var isRemoveSlide = 0;
@@ -68,7 +202,7 @@ var selectedItem = {
   item_id: '',
   bg_img: ''
 };
-interact('.draggable')
+draggable = interact('.draggable')
   .draggable({
     modifiers: [
       interact.modifiers.restrictRect({
@@ -80,6 +214,7 @@ interact('.draggable')
 
     listeners: {
       start (event) {
+        if($$('.ib01').hasClass('swiper-container-vertical')) return false; 
         //선택한 아이템 정보
         var item = swiper.slides[swiper.activeIndex].querySelectorAll('.tc01')[0];
         selectedItem.item_id = item.getAttribute('data-item-id');
@@ -89,8 +224,9 @@ interact('.draggable')
       move: dragMoveListener,
 
       end (event) {
+        if($$('.ib01').hasClass('swiper-container-vertical')) return false; 
         if(isRemoveSlide == 1){ //좋아요에 담기
-          addLike(selectedItem);  //좋아요에 추가
+          //addLike(selectedItem);  //좋아요에 추가
           setTimeout(function() {
             swiper.removeSlide(swiper.activeIndex);
             isRemoveSlide = false;
@@ -104,9 +240,10 @@ interact('.draggable')
 
       }
     }
-  })
+  });
 
 function dragMoveListener (event) {
+  if($$('.ib01').hasClass('swiper-container-vertical')) return false; 
 
   var moved = event.pageY - event.y0;
   if(moved > 200){  // 아래로 내림
@@ -134,14 +271,13 @@ function dragMoveListener (event) {
 }
 //좋아요에 추가
 function addLike (item) {
-  var elements = document.querySelectorAll('.ng02 ul li a');
-  for (let el of elements) {
-    if(el.classList.contains('empty')){
-      el.classList.remove("empty");
-      el.classList.add("thumb");
-      el.style.backgroundImage = item.bg_img;
-      el.setAttribute('href', 'view.html?item_id='+item.item_id);
-      break;
+  $$('.ng02 ul li a').each(function(){
+    if($$(this).hasClass('empty')){
+      $$(this).removeClass("empty");
+      $$(this).addClass("thumb");
+      $$(this).css('background-image',item.bg_img);
+      $$(this).attr('href', 'view.html?item_id='+item.item_id);
+      return false;
     }
-  }
+  });
 }
