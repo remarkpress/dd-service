bec7a30d18eab28263aa821881e2c105fd8434ea
Yongsun Lee <yslee264@gmail.com>
2020-09-26 18:50:34 +0900

    app.js 에서 api endpoint_hostname을 명시한다

--- js/app.js
+++ js/app.js
@@ -211,6 +211,8 @@ var app = new Framework7({
   ]
 });
 
+// var endpoint_hostname = 'http://differentdoors.durumi.io';
+var endpoint_hostname = 'http://localhost:3000';
 
 //뷰 생성
 if (localStorage["dd-member-credentials"] === undefined ) {
