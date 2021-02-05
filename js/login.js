55369113ff65d10dc34030e3091f21805c9c1429
Yongsun Lee <yslee264@gmail.com>
2021-02-05 10:32:36 +0900

    로그인 실패시 처리 수정

--- js/login.js
+++ js/login.js
@@ -17,10 +17,11 @@ $$('#login_form').on('submit', function(){
 
   app.request.post(endpoint, data, function(data) {
     var response_data = JSON.parse(data);
-    var email = response_data.data.member.email;
-    var token = response_data.data.member.authentication_token;
-
+    // console.log(response_data);
     if ( response_data.is_success === true ) {
+      var email = response_data.data.member.email;
+      var token = response_data.data.member.authentication_token;
+
       localStorage.removeItem('dd-member-credentials');
       localStorage.setItem('dd-member-credentials', JSON.stringify({ member_email: email, member_token: token }));
       localStorage.setItem('dd-member-email', email);
@@ -33,7 +34,11 @@ $$('#login_form').on('submit', function(){
         }
       });
     } else {
-      alert('오류가 있습니다.');
+      dialog_failed_login.open();
+      setTimeout(function () {
+        dialog_failed_login.close();
+        view.router.back();
+      }, 2000);
     }
   });
 });
@@ -46,4 +51,8 @@ var notification1 = app.notification.create({
   subtitle: '해당 정보를 정확하게 입력해 주세요',
   //text: 'This is a simple notification message',
   closeTimeout: 3000,
+});
+
+var dialog_failed_login = app.dialog.create({
+  text: '로그인에 실패했습니다.'
 });
